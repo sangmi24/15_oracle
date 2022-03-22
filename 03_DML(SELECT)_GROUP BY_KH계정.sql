5b7d5cc6ce26f5a908b7838db5afde46890eb7c9
sangmi <97028831+sangmi24@users.noreply.github.com>
2022-03-22 21:52:38 +0900

    add contents

--- 03_DML(SELECT)_GROUP BY_KH계정.sql
+++ 03_DML(SELECT)_GROUP BY_KH계정.sql
@@ -115,17 +115,105 @@ HAVING COUNT(BONUS) = 0;
          주의사항 : 항상 SELECT 절이 동일해야 한다!
  */
  
+ -- 1. UNION ( 합집합 - 두 쿼리문을 수행한 결과값을 더하지만 중복되는 결과는 한번만 조회 )
+ -- 부서코드가 D5이거나 또는 급여가 300만원 초과인 사원들을 조회 ( 사번, 사원명, 부서코드, 급여)
+ 
+ -- 부서코드가 D5인 사원들만 조회
+ SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+ FROM EMPLOYEE
+ WHERE DEPT_CODE = 'D5'; --> 6명 조회( 박나라, 하이유,김해술,심봉선,윤은해,대북혼)
+ 
+ -- 급여가 300만원 초과인 사원들을 조회
+ SELECT EMP_ID,EMP_NAME,DEPT_CODE,SALARY
+ FROM EMPLOYEE
+ WHERE SALARY > 3000000; --> 8명 조회(선동일,송종기,노옹철,유재식,정중하,심봉선,대북혼,전지연)
+ 
+  -- 부서코드가 D5이거나 또는 급여가 300만원 초과인 사원들을 조회 ( 사번, 사원명, 부서코드, 급여)
+ -- UNION 연산자를 이용해서 위의 두쿼리를 합체
+ SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+ FROM EMPLOYEE
+ WHERE DEPT_CODE = 'D5'
+ UNION
+ SELECT EMP_ID,EMP_NAME,DEPT_CODE,SALARY
+ FROM EMPLOYEE
+ WHERE SALARY > 3000000; -->12명 조회 (선동일,송종기,노옹철,유재식,정중하,박나라,하이유,김해술,심봉선,윤은해,대북혼,전지연)
+--  12  :  6  +  8  -  2
+-- 두 쿼리의 SELECT 절이 같아야 한다!!
 
+-- 부서코드가 D5이거나 또는 급여가 300만원 초과인 사원들을 조회 ( 사번, 사원명, 부서코드, 급여)
+-- 위의 UNION 구문 대신 하나의 SELECT 문으로 표현 ( OR 연산자 사용)
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5' OR SALARY >3000000;
+--> OR 연산자로 두 개의 조건을 엮어서 조회하면 결과는 같음!
 
+-- 2. UNION ALL ( 합집합에서 중복을 제거하지 않은 상태 == 교집합 부분이 두번 조회됨 )
+-- 부서코드가 D5이거나 또는 급여가 300만원 초과인 사원들을 조회 ( 사번, 사원명, 부서코드, 급여) => 중복제거 X
+ -- UNION ALL 연산자를 이용해서 위의 두쿼리를 합체
+ SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+ FROM EMPLOYEE
+ WHERE DEPT_CODE = 'D5'
+ UNION ALL
+ SELECT EMP_ID,EMP_NAME,DEPT_CODE,SALARY
+ FROM EMPLOYEE
+ WHERE SALARY > 3000000; 
+ -->14명 조회 ( 박나라, 하이유, 김해술, 심봉선, 윤은해,대북혼,선동일,송종기,노옹철,유재식,정중하,심봉선,대북혼,전지연)
+--  14  :  6  +  8  
+--중복제거를 하지 않기 때문에 OR연산자 사용 못함
 
+-- 3. INTERSECT ( 교집합 - 여러 쿼리 결과의 중복된 결과만을 조회)
+-- 부서 코드가 D5 이면서 그리고 급여 또한 300만원 초과인 사원들을 조회 (사번, 사원명, 부서코드, 급여)
+-- INTERSECT 연산자를 이용하여 두개의 쿼리를 합친 버전
+-- 부서 코드가 D5인 사원들
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE='D5'
+INTERSECT
+-- 급여가 300만원 초과인 사원들
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;  --> 2명 조회 (심봉선, 대북혼)
 
+-- 아래처럼도 가능
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE='D5'  AND SALARY >3000000;
 
+-- 4. MINUS ( 차집합 - 선행 쿼리 결과에서 후행 쿼리 결과를 뺀 나머지 , 순서 중요 )
+-- 부서 코드가 D5인 사원들 중에서 급여가 300만원 초과인 사원들을 제외해서 조회 (사번, 사원명, 부서코드, 급여)
+-- MINUS  연산자를 이용해서 두 쿼리를 합체
+-- 부서 코드가 D5인 사원들 (선행 쿼리)
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5' --> 6명 조회( 박나라, 하이유,김해술,심봉선,윤은해,대북혼)
+MINUS
+-- 급여가 300만원 초과인 사원들 (후행 쿼리)
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000 ; --> 8명 조회(선동일,송종기,노옹철,유재식,정중하,심봉선,대북혼,전지연)
+--> 최종 4명 조회 ( 박나라, 하이유, 김해술, 윤은해)
+-- 선행 쿼리의 결과 6명 중에서 겹치는 2명을 제외하고 4명이 조회되었다. 
 
+-- 선행 후행 순서 바꿔서 테스트
+-- 급여가 300만원 초과인 사원들 (후행 쿼리)
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000  --> 8명 조회(선동일,송종기,노옹철,유재식,정중하,심봉선,대북혼,전지연)
+MINUS
+-- 부서 코드가 D5인 사원들 (선행 쿼리)
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5' ;--> 6명 조회( 박나라, 하이유,김해술,심봉선,윤은해,대북혼)
+--> 최종 6명 조회 ( 선동일, 송종기, 노옹철,유재식,정중하,전지연)
+--> 선행 쿼리의 결과 8명 중에서 겹치는 2명을 제외하고 6명이 조회되었다.
 
-
-
-
-
+--아래처럼도 가능
+-- 부서 코드가 D5 인 사원들 중에서 급여가 300만원 초과인 사원들을 제외해서 조회 (사번, 사원명, 부서코드, 급여)
+-- 부서 코드가 D5 인 사원들 중에서 급여가 300만원 이하인 사원들만 조회 (사번, 사원명, 부서코드, 급여)
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE='D5' AND SALARY <= 3000000;
+--> 최종 4명 조회 ( 박나라, 하이유, 김해술, 윤은해)
 
 
 
