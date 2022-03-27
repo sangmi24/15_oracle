d522c9aa057b7cba9bdad3b005159a1d1f67f6ac
sangmi <97028831+sangmi24@users.noreply.github.com>
2022-03-27 23:46:06 +0900

    add practice

--- 01_PRACTICE2.sql
+++ 01_PRACTICE2.sql
@@ -127,7 +127,8 @@ ORDER BY E.SALARY DESC;
 --EMPLOYEE 테이블에서 부서별 그룹을 편성하여
 --부서별 급여 합계, 제일 낮게 받는 부서와, 제일 높게 받는부서, 인원수를 조회
 --단, 조회결과는 인원수 오름차순하여 출력하여라.
---서브쿼리 이용
+--서브쿼리 이용  -- 코드 참고함! 처음에는 제일 낮게 받는 급여로 해석해서 풀다가 
+--부서로 나와야 할것같아서 서브쿼리로 연결해서 코드 붙임 DECODE를 이용해서 나열함 
 SELECT DEPT_CODE 부서 ,SUM(SALARY) "부서별급여합계"
           , DECODE(SUM(SALARY), (SELECT MAX(SUM(SALARY)) 
                                                FROM EMPLOYEE
@@ -135,72 +136,202 @@ SELECT DEPT_CODE 부서 ,SUM(SALARY) "부서별급여합계"
                                               ,(SELECT MIN(SUM(SALARY))
                                                 FROM EMPLOYEE
                                                 GROUP BY DEPT_CODE), '가장 적게 받는 부서','기타') 부서급여
-        ,COUNT(*)
+        ,COUNT(*) --COUNT(*)은 NULL값과 상관없이 모든 행 수를 카운트한다. 
 FROM EMPLOYEE
 GROUP BY DEPT_CODE
 ORDER BY COUNT(*) ASC;
                                                 
-                            
-
-
-
-
 
 -----------------------------------------------------------------------------------
 --[문제12]
 --EMPLOYEE 테이블에서 직급별
 --그룹을 편성하여 직급코드, 급여평균, 급여합계, 인원 수를 조회
 --단, 조회 결과는 급여평균 오름차순하여 출력, 인원수는 3명을 초과하는 직급만 조회
+--1) 3명 초과하는 직급만 조회-J7,J6,J4
+SELECT JOB_CODE
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+HAVING COUNT(*) >3; --COUNT는 그룹함수라서 GROUP BY를 써줘야 함 / *은  NULL과 관계없이 나올수 있기 때문에 씀 
+
+--2) 최종
+--다중행 단일열 서브쿼리 IN :여러개의 결과값 중에서 한개라도 일치하는 값이 있다면.. 나옴
+SELECT JOB_CODE 직급코드, ROUND(AVG(SALARY)) 급여평균, SUM(SALARY) 급여합계, COUNT(*)  인원수
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+HAVING JOB_CODE IN ( SELECT JOB_CODE
+                               FROM EMPLOYEE
+                               GROUP BY JOB_CODE
+                               HAVING COUNT(*) >3 );
 -----------------------------------------------------------------------------------
 --[문제13]
 --2001년에 입사한 여자 직원이 있다.
 --해당 직원과 같은 부서, 같은 직급에 해당하는 사원들을 조회하시오.
 --사번, 사원명, 직급, 부서, 입사일
+--1) 2001년에 입사한 여자 직원 구하기 --윤은해
+SELECT EMP_NAME
+FROM EMPLOYEE
+WHERE SUBSTR(HIRE_DATE,1,2)=01
+AND SUBSTR(EMP_NO,8,1) IN( 2, 4)  ;
+
+--2) 윤은해 부서와 직급코드를 알아내야함 --D5, J7
+SELECT DEPT_CODE, JOB_CODE
+FROM EMPLOYEE
+WHERE EMP_NAME=(  SELECT EMP_NAME
+                                FROM EMPLOYEE
+                                WHERE SUBSTR(HIRE_DATE,1,2)=01
+                                AND SUBSTR(EMP_NO,8,1) IN( 2, 4) ) ;
+                                
+--3) 최종 합치기
+SELECT EMP_ID 사번,EMP_NAME 사원명, JOB_CODE 직급, DEPT_CODE 부서, HIRE_DATE 입사일
+FROM EMPLOYEE
+WHERE (DEPT_CODE,JOB_CODE) 
+  = ( SELECT DEPT_CODE, JOB_CODE
+      FROM EMPLOYEE
+      WHERE EMP_NAME=(  SELECT EMP_NAME
+                                       FROM EMPLOYEE
+                                      WHERE SUBSTR(HIRE_DATE,1,2)=01
+                                       AND SUBSTR(EMP_NO,8,1) IN( 2, 4) )  );
+
 -----------------------------------------------------------------------------------
 --[문제14]
 --EMPLOYEE 테이블에서 '하이유'와 같은 부서에서 일하는 사원들의 
---사원번호, 사원명, 부서코드 직급코드, 급여 조회
+--사원번호, 사원명, 부서코드, 직급코드, 급여 조회
 --직급코드 내림차순 조회
+
+--1) 하이유의 부서를 알아내기 --D5
+ SELECT DEPT_CODE
+ FROM EMPLOYEE
+ WHERE EMP_NAME= '하이유';
+ --2) 최종
+SELECT EMP_ID 사원번호 ,EMP_NAME 사원명, DEPT_CODE 부서코드, JOB_CODE 직급코드, SALARY 급여
+FROM EMPLOYEE
+WHERE DEPT_CODE = ( SELECT DEPT_CODE
+                                 FROM EMPLOYEE
+                                 WHERE EMP_NAME= '하이유' );
 -----------------------------------------------------------------------------------
 --[문제15]
---EMPLOYEE 테이블에서 입사일이 2000년 1월 1일 이전인 사원에 대해 사원의 이름,  입사일,  부서코드, 급여를 입사일순으로 조회하시오
+--EMPLOYEE 테이블에서 입사일이 2000년 1월 1일 이전인 사원에 대해 
+--사원의 이름,  입사일,  부서코드, 급여를 입사일순으로 조회하시오
 --(문제에 있는 이름대로 컬럼명을 따로 붙여주세요)
+--2000년 1월 1일이전에 입사한 사람이 없다. 
+SELECT EMP_NAME "사원의 이름", HIRE_DATE "입사일", DEPT_CODE "부서코드",SALARY "급여"
+FROM EMPLOYEE
+WHERE TO_CHAR(HIRE_DATE,'YYYYMMDD') < TO_DATE('20000101','YYYYMMDD') ;
 -----------------------------------------------------------------------------------
 --[문제16]
 --EMPLOYEE 테이블에서 해외영업 부서(DEPT_TITLE) 소속인 사원들의
---이름(EMP_NAME), 직급(JOB_TITLE), 부서명(DEPT_TITLE), 근무국가(NATIONAL_CODE)를 조회하시오
+--이름(EMP_NAME), 직급(JOB_CODE), 부서명(DEPT_TITLE), 근무국가(NATIONAL_CODE)를 조회하시오
 --단, 오라클 조인 구문으로 작성하고 별칭을 반드시 입력
+--근무국가가 조회가 되어야 한다. => LOCATION 테이블에 있음
+--연결고리 정리 
+--1) E.DEPT_CODE= D.DEPT_ID  ( 예. D9)
+--2) D.LOCATION_ID=N.LOCAL_CODE (예.L1)
+--3)  L.NATIONAL_CODE =N.NATIONAL_CODE (예. KO  한국) X
+SELECT E.EMP_NAME 이름, E.JOB_CODE 직급, D.DEPT_TITLE 부서명 
+        , L.NATIONAL_CODE 근무국가
+FROM EMPLOYEE E , DEPARTMENT D, LOCATION L
+WHERE E.DEPT_CODE=D.DEPT_ID  (+)  --연결고리 지어줌 
+AND D.LOCATION_ID=L.LOCAL_CODE
+AND D.DEPT_TITLE LIKE '해외영업%'; --조건 지어줌 
 -----------------------------------------------------------------------------------
 --[문제17]
 --EMPLOYEE 테이블에서
 --'이태림'사원의 근속 년수를 조회하시오 (현재는 퇴사상태)
+-- 오라클에서 제공하는  months_between 함수를 이용함 --나머지는 버리는 TRUNC숫자함수이용 
+SELECT TRUNC(MONTHS_BETWEEN( ENT_DATE, HIRE_DATE )/12) "근속년"
+FROM EMPLOYEE
+WHERE EMP_NAME='이태림';
 -----------------------------------------------------------------------------------
 --[문제18]
---자신이 속한 직급의 평균 급여보다 많이 받는 사원의
+--자신이 속한 직급의 평균 급여보다 많이 받는 사원의 JOB_CODE
 --사원번호,직급명, 사원명,부서명, 급여정보 조회
+--1) 같은직급끼리 평균 급여 구하기 
+--대표: 8000000/대리: 2624373/차장: 2330000/과장:2820000/부사장:4850000/부장:3600000/사원:2017500
+SELECT J.JOB_NAME ,J.JOB_CODE  CODE, ROUND(AVG(E.SALARY)) AVGS 
+FROM EMPLOYEE E, JOB J
+WHERE E.JOB_CODE=J.JOB_CODE
+GROUP BY J.JOB_NAME, J.JOB_CODE;
+--최종
+SELECT E.EMP_ID 사원번호, E.EMP_NAME 사원명, A.JOB_NAME 직급명
+           , D. DEPT_TITLE 부서명,E.SALARY 급여, A.AVGS 부서평균급여
+FROM EMPLOYEE E, DEPARTMENT D,
+          (SELECT J.JOB_NAME ,J.JOB_CODE CODE, ROUND(AVG(E.SALARY)) AVGS 
+            FROM EMPLOYEE E, JOB J
+            WHERE E.JOB_CODE=J.JOB_CODE
+            GROUP BY J.JOB_NAME, J.JOB_CODE ) A
+WHERE E.DEPT_CODE=D.DEPT_ID  --연결고리 연결해줌 E테이블이랑 D테이블
+   AND  E.JOB_CODE=A.CODE  --E테이블이랑 A안에 있는 JOB테이블 
+   AND E.SALARY >A.AVGS;   -- 조건 :  E테이블에서 급여 > 1단계에서 구한 평균 급여 
+                                         --자신이 속한 직급의 평균 급여보다 많이 받는 사원 
 -----------------------------------------------------------------------------------
 --[문제19]
 --부서별로 근무하는 사원의 수가 3명 이하인 경우, 사원이 적은 부서별로 오름차순 정렬 조회
+SELECT DEPT_CODE 부서, COUNT(*) 사원수
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING COUNT(*) <= 3 --NULL 인것은 *로 표현해서 한다. 
+ORDER BY COUNT(*) ASC;
+
 -----------------------------------------------------------------------------------
 --[문제20]
 --EMPLOYEE 테이블에서
 --직급 별로 급여평균을 조회하고 급여평균 내림차순으로 정렬하시오
 --(급여평균은 TRUNC 함수 사용하여 만원단위 이하는 버림 하시오)
+SELECT  JOB_CODE,  TRUNC(AVG(SALARY),-4  )
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+ORDER BY AVG(SALARY) DESC;
 -----------------------------------------------------------------------------------
 --[문제21]
 --해외영업2부(DEPT_CODE: D6)의 평균 급여보다 많이 받고, 해외영업2부에 속하지 않으며 
 --관리자가 없는 사원의 사번(EMP_ID), 이름(EMP_NAME), 직급(JOB_NAME), 부서이름(DEPT_TITLE), 급여(SALARY)를 조회하시오.
 --단,FROM 절에 서브쿼리 사용, JOIN은 오라클 구문 사용, 셀프 조인 사용
+--1) 해외영업2부의 평균 급여--3366666
+SELECT E.DEPT_CODE, TRUNC(AVG(E.SALARY))
+FROM EMPLOYEE E, DEPARTMENT D
+WHERE E.DEPT_CODE=D.DEPT_ID
+GROUP BY E.DEPT_CODE
+HAVING E.DEPT_CODE='D6';
+--2) 합치기 
+SELECT E.EMP_ID 사번, E.EMP_NAME 이름, J.JOB_NAME 직급, A.DEPT_TITLE 부서이름, E.SALARY 급여
+FROM EMPLOYEE E , JOB J,
+          (SELECT E.DEPT_CODE, TRUNC(AVG(E.SALARY)) AVGS
+          FROM EMPLOYEE E, DEPARTMENT D
+          WHERE E.DEPT_CODE=D.DEPT_ID
+          GROUP BY E.DEPT_CODE
+         HAVING E.DEPT_CODE='D6')  A
+WHERE E.JOB_CODE=J.JOB_CODE
+AND E.DEPT_CODE=A.DEPT_ID       
+AND E.SALARY > A.AVGS    ;  
+--오류.. 잘모르게씀..
 -----------------------------------------------------------------------------------
 --[문제22] 
---EMP에서 직급이름으로 그룹을 만들고 월급이 5000이상인 그룹 찾기
+--EMP에서 직급이름으로 그룹을 만들고 월급이 500만원이상인 그룹 찾기
 --JOB이름과, 급여 합계를 조회하시오
+SELECT JOB_NAME 직급명, SUM(SALARY) 급여합계
+FROM EMPLOYEE E, JOB J
+WHERE E.JOB_CODE=J.JOB_CODE
+GROUP BY JOB_NAME
+HAVING SUM(SALARY)>5000000;
+
 -----------------------------------------------------------------------------------
 --[문제23]
 --EMPLOYEE 테이블에서
 --입사일로부터 근무년수가 가장 긴 직원 상위 6명을
 --RANK()함수를 이용하여 조회하시오
 --사번, 사원명, 부서명, 직급명, 입사일을 조회하시오.
+--1) 순위나타내기 
+SELECT E.EMP_ID 사번, E.EMP_NAME 사원명, J.JOB_NAME 직급명
+           ,RANK() OVER(ORDER BY SYSDATE-HIRE_DATE DESC) 순위
+FROM EMPLOYEE E, JOB J
+WHERE E.JOB_CODE=J.JOB_CODE;
+--2) 순번과 순위6명까지  잘라내기  
+SELECT ROWNUM, E.*
+FROM (SELECT E.EMP_ID 사번, E.EMP_NAME 사원명, J.JOB_NAME 직급명
+           ,RANK() OVER(ORDER BY SYSDATE-HIRE_DATE DESC) 순위
+            FROM EMPLOYEE E, JOB J
+            WHERE E.JOB_CODE=J.JOB_CODE) E
+WHERE 순위 <=6;            
 -----------------------------------------------------------------------------------
 --[문제24]
 --EMPLOYEE 테이블에서 
